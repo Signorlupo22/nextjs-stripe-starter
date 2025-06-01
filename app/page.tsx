1d048b444745738d3cebecdbc2db30bc5bedfcb6
Signorlupo22 <faverothomas04@gmail.com>
2025-06-01 16:32:24 +0200

    Update home page title to reflect project focus on Stripe integration

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,10 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={"/"}>Next.js Supabase and stripe Starter</Link>
               <div className="flex items-center gap-2">
-              </div>
+              </div>  
             </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
