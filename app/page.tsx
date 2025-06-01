22f57140544d81ef7628e8cea20170b56fb1f710
Signorlupo22 <faverothomas04@gmail.com>
2025-06-01 16:39:02 +0200

    Refactor home page by removing unused components and adding account creation and sign-in buttons. Update footer to reflect new project attribution.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
@@ -21,20 +18,30 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-          </main>
+          <div className="flex gap-2 justify-center">
+            <Link href={"/auth/sign-up"}>
+              <Button>
+                Create an account
+              </Button>
+            </Link>
+            <Link href={"/auth/login"}>
+              <Button>
+                Sign in
+              </Button>
+            </Link>
+          </div>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
             Powered by{" "}
             <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              href="https://github.com/Signorlupo22"
               target="_blank"
               className="font-bold hover:underline"
               rel="noreferrer"
             >
-              Supabase
+              Thomas Favero
             </a>
           </p>
           <ThemeSwitcher />
